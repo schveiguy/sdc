d3bbe41cdb7711907aa1a06ff9c538fa25d80fd8
Steven Schveighoffer <schveiguy@gmail.com>
2024-06-23 17:45:16 -0400

    Support for removing ranges/roots and adding roots (pointer to a block that should not be collected).

--- sdlib/d/gc/global.d
+++ sdlib/d/gc/global.d
@@ -25,6 +25,13 @@ public:
 		(cast(GCState*) &this).addRootsImpl(range);
 	}
 
+	void removeRoots(const void* ptr) shared {
+		mutex.lock();
+		scope(exit) mutex.unlock();
+
+		(cast(GCState*) &this).removeRootsImpl(ptr);
+	}
+
 	void scanRoots(ScanDg scan) shared {
 		mutex.lock();
 		scope(exit) mutex.unlock();
@@ -46,14 +53,46 @@ private:
 		roots = (cast(const(void*)[]*) ptr)[0 .. length];
 
 		import d.gc.range;
-		roots[index] = makeRange(range);
+		if (range.length == 0)
+			roots[index] = cast(void*[]) range;
+		else
+			roots[index] = makeRange(range);
+	}
+
+	void removeRootsImpl(const void* ptr) {
+		assert(mutex.isHeld(), "Mutex not held!");
+
+		auto rptr = cast(void*) roots.ptr;
+
+		foreach (i; 0 .. roots.length) {
+			if (cast(void*) roots[i].ptr == ptr) {
+				auto length = roots.length - 1;
+				roots[i] = roots[length];
+				import d.gc.tcache;
+				rptr = threadCache.realloc(rptr, length * void*[].sizeof, true);
+				roots = (cast(const(void*)[]*) rptr)[0 .. length];
+				return;
+			}
+		}
 	}
 
 	void scanRootsImpl(ScanDg scan) {
 		assert(mutex.isHeld(), "Mutex not held!");
 
-		foreach (range; roots) {
-			scan(range);
+		foreach (ref range; roots) {
+			if (range.length == 0) {
+				// range is actually just a root pointer. We need to pass in
+				// the pointer itself as an array, but cannot use the stack for
+				// this.
+				static struct Array {
+					size_t length;
+					void* ptr;
+				}
+
+				Array* arr = cast(Array*) &range;
+				scan((&arr.ptr)[0 .. 1]);
+			} else
+				scan(range);
 		}
 	}
 }
