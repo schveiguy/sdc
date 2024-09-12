6daa1ad7e99554721ff3a9920d7ca0f0091015ff
Steven Schveighoffer <schveiguy@gmail.com>
2024-09-12 13:49:17 -0400

    Fixup things. Zero tail of pointer-containing memory. Hack to make trampoline work.

--- sdlib/d/gc/tcache.d
+++ sdlib/d/gc/tcache.d
@@ -129,8 +129,7 @@ public:
 			return allocSmall(size, containsPointers, zero);
 		}
 
-		auto pages = getPageCount(size);
-		return allocLarge(pages, containsPointers, zero);
+		return allocLarge(size, containsPointers, zero);
 	}
 
 	void* allocAppendable(size_t size, bool containsPointers, bool zero,
@@ -157,8 +156,7 @@ public:
 			return ptr;
 		}
 
-		auto pages = getPageCount(size);
-		auto ptr = allocLarge(pages, containsPointers, zero);
+		auto ptr = allocLarge(size, containsPointers, zero);
 		if (unlikely(ptr is null)) {
 			return null;
 		}
@@ -327,6 +325,9 @@ private:
 
 		if (unlikely(zero)) {
 			memset(ptr, 0, slotSize);
+		} else if (containsPointers) {
+			// Zero out the non-allocated part of the slot to avoid stale pointers.
+			memset(ptr + size, 0, slotSize - size);
 		}
 
 		triggerAllocationEvent(slotSize);
@@ -399,10 +400,10 @@ private:
 	/**
 	 * Large allocations.
 	 */
-	void* allocLarge(uint pages, bool containsPointers, bool zero) {
+	void* allocLarge(size_t size, bool containsPointers, bool zero) {
 		// We are about to allocate, make room for it if needed.
 		maybeRunGCCycle();
-
+		auto pages = getPageCount(size);
 		void* ptr;
 
 		{
@@ -415,6 +416,10 @@ private:
 
 		if (unlikely(ptr is null)) {
 			return null;
+		}
+
+		if (containsPointers && !zero) {
+			memset(ptr + size, 0, pages * PageSize - size);
 		}
 
 		triggerAllocationEvent(pages * PageSize);
